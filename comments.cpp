4b1c5740a741f6832c9680d17b89ab6969f113bf
vezeveer <mortalmanny@live.com>
2019-10-17 14:59:16 +0800

    completed before adding interface

--- comments.cpp
+++ comments.cpp
@@ -1,143 +1,176 @@
-//Description: Continuously asks if the user wants to do an operation on the specified number
+//Description: Performs number operations as many times as required
 //Programmers: Emmanuel Valdueza, Jake P Ogsimer, Mark L Perez
 //Created: Oct-02-2019 | Oct-08-2019
 
-#include <iostream> //library for using cin cout
+#include <iostream> //library for using cout, cin
 #include <limits>   //library for handling cin unwanted input
 #include <string>   //library for using string
 
-//global variables
-double currentNumber = 0.0;
-bool enteredARealNumber = false, firstNumValid = false, operaIsInvalid = true;
+using std::cin; //prevents identifier naming conflicts
+using std::cout;
+using std::string;
 
-//forward declarations. Needed because of C++ top down approach.
-std::string displayMode(char op);               //changes text depending on operation asked
-char convertToChar(std::string opera);          //required because switch only takes in char or number
-bool isItANumber();                             //checks if input is a number
-double calculation(char operation, double num); //performs the operation depending on what is asked
+string operationAsked; //global variables 13-15
+bool invalidOp = true, repeatedOperation = false;
+double secondNum, result, firstNum;
 
-int main() //entry point of program
+string changeWords(string operation);                            //function declarations through lines 17-25
+double calculate(string operation, double first, double second); //takes in 2 numbers and an operation, and returns a result
+void promptToExit();
+bool isItAValidOperation(string op); //gets the operation, returns false if not any of the operations asked
+bool isItAValidNumber(bool yesNo);   //basically, it checks if current input is a number. Returns false if not.
+double askNumber(bool);              //Gets number and returns it if it is valid. Boolean argument changes sentence
+void showResults();
+void welcomeMessage();
+void askOperation(); //asks for number and stores it to global variable
+
+int main() //main entry point of program
 {
-  //local variables
-  double numFollowUp;
-  char op;
-  std::string operaString, doAgain = "n";
-
-  //welcome note
-  std::cout << "Welcome. I will ask for a number and you will\n"
-            << "tell me what operation to perform on another number\n"
-            << "that I will ask you. We will then do another operation \n"
-            << "on the result if you wish to continue.\n\n";
-
-  //wll continue asking for a number if input is not a number
   do
   {
-    std::cout << "Enter a number: ";
-    std::cin >> currentNumber;
-
-    if (isItANumber())
+    if (!repeatedOperation) //will occur only once
     {
-      while (operaIsInvalid)
-      {
-        std::cout << "Enter an operation \"+, -, / or *\": ";
+      welcomeMessage();
+      firstNum = askNumber(repeatedOperation);
+      askOperation();
+    }
 
-        std::cin >> operaString;
-        op = convertToChar(operaString);
+    secondNum = askNumber(true);
+    result = calculate(operationAsked, firstNum, secondNum);
 
-        if (op == '+' || op == '-' || op == '/' || op == '*')
-        {
-          do
-          {
-            firstNumValid = false;
-            std::cout << "Enter a number " << displayMode(op) << currentNumber << ": ";
-            std::cin >> numFollowUp;
-            //verify input is a number
-            if (isItANumber())
-            {
-              std::cout << "\nResult: " << calculation(op, numFollowUp)
-                        << "\n\nType \"yes\" to do another opertaion on the result. ";
-              std::cin >> doAgain;
-              operaIsInvalid = false;
-              if (doAgain == "yes")
-              {
-                std::cout << std::endl;
-                operaIsInvalid = true;
-              }
-            }
-            else
-            {
-              std::cout << "Input is not a number. ";
-            }
-          } while (firstNumValid == false);
-        }
-        else
-        {
-          operaIsInvalid = true;
-          std::cout << "Invalid operator. ";
-        }
-      }
+    showResults();
+    promptToExit();
+
+    cin >> operationAsked;
+    if (isItAValidOperation(operationAsked))
+    {
+      firstNum = result;
+      repeatedOperation = true;
     }
     else
     {
-      std::cout << "Input is not a number. ";
+      cout << "\n# You chose to exit the program. #\n\n";
+      repeatedOperation = false;
     }
-  } while (firstNumValid == false);
+  } while (repeatedOperation); //loops operation as long as repeated is true
 
-  std::cout << "\n\n";
-  system("pause");
+  system("pause"); //without this the program would close immediately if told to exit
   return 0;
 }
 
-char convertToChar(std::string opera)
+void welcomeMessage()
 {
-  return opera[0]; //returns first char of word
+  cout << "\nThis program will perform one or more "
+       << "operations as required. \n\n";
 }
 
-bool isItANumber()
+void askOperation()
 {
-  if (std::cin.fail()) //check if input is a number
+  do
   {
-    std::cin.clear();                                                   //clears cin errors
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //clears cin input
+    cout << "Type +, -, * or /: ";
+    cin >> operationAsked;
+    if (!isItAValidOperation(operationAsked))
+      cout << "Invalid. Please ";
+  } while (!isItAValidOperation(operationAsked));
+}
+
+void showResults()
+{
+  cout << "\nResult: "
+       << firstNum << " "
+       << operationAsked << " "
+       << secondNum << " = "
+       << result << "\n";
+}
+
+double askNumber(bool repeatedOpera)
+{
+  double num;
+  do
+  {
+    if (repeatedOpera)
+    {
+      cout << "\nEnter number "
+           << changeWords(operationAsked)
+           << firstNum << ": ";
+    }
+    else
+      cout << "\nEnter number: ";
+    cin >> num;
+  } while (!isItAValidNumber(cin.fail()));
+  return num;
+}
+
+double calculate(string operation, double first, double second)
+{
+  if (operation == "+")
+    return first + second;
+  if (operation == "-")
+    return first - second;
+  if (operation == "*")
+    return first * second;
+  if (operation == "/")
+    return first / second;
+
+  return 0;
+}
+
+bool isItAValidOperation(string op)
+{
+  if (op == "+" || op == "-" || op == "*" || op == "/")
+    return true;
+  else
+  {
+    return false;
+  }
+}
+
+string changeWords(string operation)
+{
+  if (operation == "+")
+    return "to add to ";
+  if (operation == "-")
+    return "to subtract from ";
+  if (operation == "*")
+    return "to multiply with ";
+  if (operation == "/")
+    return "to divide ";
+
+  return ""; //will never reach
+}
+
+void promptToExit()
+{
+  cout << "\nType the ff operations."
+       << "\nInvalid input will exit program."
+       << "\n+, -, *, /:";
+}
+
+bool isItAValidNumber(bool yesNo)
+{
+  if (yesNo)
+  {
+    cin.clear(); //resets cin.fail() to false
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    /*
+      cin.ignore() clears the characters that would make 
+			cin.fail() true where the first argument indicates no
+			limit to the number of characters to ignore and the
+			second argument the character from which to stop ignoring
+      which is the character for enter.
+    */
+    cout << "Invalid input. ";
     return false;
   }
   else
   {
-    firstNumValid = 1;
+    /*
+      Discards extra input.
+      If user types in numbers then letters, the
+      letters and everything after will be discarded.
+    */
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     return true;
-  }
-}
-
-double calculation(char operation, double num)
-{
-  switch (operation)
-  {
-  case '+':
-    return currentNumber = currentNumber + num;
-  case '-':
-    return currentNumber = currentNumber - num;
-  case '*':
-    return currentNumber = currentNumber * num;
-  case '/':
-    return currentNumber = currentNumber / num;
-  default:
-    return 404;
-  }
-}
-
-std::string displayMode(char op)
-{
-  switch (op)
-  {
-  case '+':
-    return "to add to ";
-  case '-':
-    return "to subtract from ";
-  case '*':
-    return "to multiply with ";
-  case '/':
-    return "to divide ";
-  default:
-    return "[Not Valid]";
   }
 }
--- main2.cpp
+++ main2.cpp
@@ -1,7 +1,196 @@
-#include <iostream> //indicates get code from file iostream
+//Description: Performs number operations as many times as required
+//Programmers: Emmanuel Valdueza, Jake P Ogsimer, Mark L Perez
+//Created: Oct-02-2019 | Oct-08-2019
 
-int main()											//indicates program starts here
-{																//indicates start of block of code
-	std::cout << "Hello, world!"; //indicates print Hello, world!
-	return 0;											//indicates everything is ok and exits
-} //indicates end of block of code
+#include <iostream>
+#include <limits> //library for handling cin unwanted input
+#include <string>
+
+using std::cin; //prevents identifier naming conflicts
+using std::cout;
+using std::string;
+
+string operationAsked;
+bool invalidOp = true, repeatedOperation = false;
+double secondNum, result, firstNum;
+
+string changeWords(string operation);
+double calculate(string operation, double first, double second);
+void promptToExit();
+bool isItAValidOperation(string op);
+bool isItAValidNumber(bool yesNo);
+double askNumber(bool);
+void showResults();
+void welcomeMessage();
+void askOperation(bool repeatedOpera);
+void display();
+
+int main()
+{
+	display();
+	do
+	{
+		if (!repeatedOperation)
+		{
+			welcomeMessage();
+			firstNum = askNumber(repeatedOperation);
+			askOperation(repeatedOperation);
+		}
+
+		secondNum = askNumber(true);
+		result = calculate(operationAsked, firstNum, secondNum);
+
+		showResults();
+		promptToExit();
+
+		cin >> operationAsked;
+		if (isItAValidOperation(operationAsked))
+		{
+			firstNum = result;
+			repeatedOperation = true;
+		}
+		else
+		{
+			cout << "\n\n\n# You chose to exit the program. #\n\n\n\n";
+			repeatedOperation = false;
+		}
+	} while (repeatedOperation);
+
+	system("pause");
+	return 0;
+}
+
+void display()
+{
+	system("CLS");
+	cout << "\n##                                                                   ## "
+			 << "\n# This program will perform one or more calculations as required  #"
+			 << "\n##                                                                   ##"
+			 << "\n"
+			 << "\nLeft Hand Number: _"
+			 << "\nOperation Symbol: _"
+			 << "\nRight Hand Number: _"
+			 << "\nResult: _"
+			 << "\n"
+			 << "\nEnter Left Hand Number: ";
+}
+
+void welcomeMessage()
+{
+	cout << "\nThis program will perform one or more "
+			 << "operations as required. \n\n";
+}
+
+void askOperation(bool repeatedOpera)
+{
+	if (!repeatedOpera)
+	{
+		do
+		{
+			cout << "Type +, -, * or /: ";
+			cin >> operationAsked;
+			if (!isItAValidOperation(operationAsked))
+				cout << "Please ";
+		} while (!isItAValidOperation(operationAsked));
+	}
+}
+
+void showResults()
+{
+	cout << "\n    Result: "
+			 << firstNum << " "
+			 << operationAsked << " "
+			 << secondNum << " = "
+			 << result << "\n";
+}
+
+double askNumber(bool repeatedOpera)
+{
+	double num;
+	do
+	{
+		if (repeatedOpera)
+		{
+			cout << "\nEnter number "
+					 << changeWords(operationAsked)
+					 << firstNum << ": ";
+		}
+		else
+			cout << "\nEnter number: ";
+		cin >> num;
+	} while (!isItAValidNumber(cin.fail()));
+	return num;
+}
+
+double calculate(string operation, double first, double second)
+{
+	if (operation == "+")
+		return first + second;
+	if (operation == "-")
+		return first - second;
+	if (operation == "*")
+		return first * second;
+	if (operation == "/")
+		return first / second;
+
+	return 0;
+}
+
+bool isItAValidOperation(string op)
+{
+	if (op == "+" || op == "-" || op == "*" || op == "/")
+		return true;
+	else
+	{
+		return false;
+	}
+}
+
+string changeWords(string operation)
+{
+	if (operation == "+")
+		return "to add to ";
+	if (operation == "-")
+		return "to subtract from ";
+	if (operation == "*")
+		return "to multiply with ";
+	if (operation == "/")
+		return "to divide ";
+
+	return ""; //will never reach
+}
+
+void promptToExit()
+{
+	cout << "\nType any of the ff operations."
+			 << "\nInvalid input will exit program."
+			 << "\n+, -, *, /:";
+}
+
+bool isItAValidNumber(bool yesNo)
+{
+	if (yesNo)
+	{
+		cin.clear(); //resets cin.fail() to false
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		/*
+      cin.ignore() clears the characters that would make 
+			cin.fail() true where the first argument indicates no
+			limit to the number of characters to ignore and the
+			second argument the character from which to stop ignoring
+      which is the character for enter.
+    */
+		cout << "Invalid input. ";
+		return false;
+	}
+	else
+	{
+		/*
+      Discards extra input.
+      If user types in numbers then letters, the
+      letters and everything after will be discarded.
+    */
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		return true;
+	}
+}
